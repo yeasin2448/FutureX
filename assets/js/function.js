c44613ce7af81f3d5e99b058a30c5d70160665f8
yeasin2448 <ayeasin108@gmail.com>
2020-10-27 00:18:58 +0600

    add two html pages

--- assets/js/function.js
+++ assets/js/function.js
@@ -119,6 +119,19 @@
 			return false;
 		});
 	});
+
+	/*-------------------------------------
+    password Toggle Class
+    -------------------------------------*/
+    $(".toggle-password").on('click', function() {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
     
 
 })(jQuery);
